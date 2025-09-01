721054dcdd50311367c3a5872fe0539c90447fe4
Enrico Weigelt, metux IT consult <info@metux.net>
2025-09-01 12:16:25 +0200

    1

---
+++ scripts/cleanup-repos.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Resolve the script path even if it's a symlink
+get_script_dir() {
+    local src="${BASH_SOURCE[0]}"
+    while [ -h "$src" ]; do
+        local dir
+        dir="$(cd -P "$(dirname "$src")" >/dev/null 2>&1 && pwd)"
+        src="$(readlink "$src")"
+        [[ "$src" != /* ]] && src="$dir/$src"
+    done
+    cd -P "$(dirname "$src")" >/dev/null 2>&1 && pwd
+}
+
+SCRIPT_DIR="$(get_script_dir)"
+
+purge_repos() {
+    for repo in "$@" ; do
+        echo "Purging repo: $repo"
+        $SCRIPT_DIR/gh-purge-deleted-branch-workflows.sh "X11Libre/$repo"
+        $SCRIPT_DIR/gh-purge-pipelines.sh --org X11Libre --repo $repo --branch master --keep 3
+    done
+}
+
+purge_repos \
+    xserver \
+    xf86-video-amdgpu
+
+XSERVER_CLEAN_BRANCHES="
+    master
+    maint-25.0
+    rfc/new-abi-25.1
+    wip/cleanup
+    wip/requests
+    wip/xinerama-walkScreen
+    wip/swapping_new
+"
+
+for i in $XSERVER_CLEAN_BRANCHES ; do
+    $SCRIPT_DIR/gh-purge-pipelines.sh --org X11Libre --repo $repo --branch master --keep 3
+done
