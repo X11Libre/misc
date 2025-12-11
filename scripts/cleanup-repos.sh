07a713ce828f00be651a7243312d34df84013ae6
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-11 18:52:21 +0100

    1

--- scripts/cleanup-repos.sh
+++ scripts/cleanup-repos.sh
@@ -16,7 +16,7 @@ get_script_dir() {
 
 SCRIPT_DIR="$(get_script_dir)"
 
-purge_repos() {
+purge_repos_master() {
     for repo in "$@" ; do
         echo "Purging repo: $repo"
         $SCRIPT_DIR/gh-purge-deleted-branch-workflows.sh "X11Libre/$repo"
@@ -24,20 +24,21 @@ purge_repos() {
     done
 }
 
-purge_repos \
+purge_repos_master \
     xserver \
     xf86-video-amdgpu
 
 XSERVER_CLEAN_BRANCHES="
     master
     maint-25.0
+    wip/swapping_new
     rfc/new-abi-25.1
     wip/cleanup
     wip/requests
     wip/xinerama-walkScreen
-    wip/swapping_new
 "
 
-for i in $XSERVER_CLEAN_BRANCHES ; do
-    $SCRIPT_DIR/gh-purge-pipelines.sh --org X11Libre --repo $repo --branch master --keep 3
+for branch in $XSERVER_CLEAN_BRANCHES ; do
+    echo "Purging X11Libre/xserver branch $branch"
+    $SCRIPT_DIR/gh-purge-pipelines.sh --org X11Libre --repo xserver --branch $branch --keep 3 || true
 done
